14854b3a404e7628738235e906d8bac1a58d5de7
Quang Duc <vuquangduc1404@gmail.com>
2024-12-18 16:54:12 +0700

    video31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,9 +3,19 @@ import "./style.css"
 
 // component = html + css + js
 const MyComponent = () => {
+    // const hoidanit = "eric"; // string
+    // const hoidanit = 25;     // number
+    // const hoidanit = true;   // boolean
+    const hoidanit = [1, 2, 3];
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 25
+    // }
+
     return (
         <>
-            <div>Eric & HoiDanIT</div>
+            <div> {JSON.stringify(hoidanit)} Eric & HoiDanIT</div>
+            <div>{console.log("Eirc")}</div>
             <div className="child"
                 style={{ borderRadius: "10px" }}
             >child</div>
