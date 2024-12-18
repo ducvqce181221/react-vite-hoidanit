157932ad3091859d0ef767eab7736a89c926e807
Quang Duc <vuquangduc1404@gmail.com>
2024-12-18 16:27:43 +0700

    video30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,16 @@
+
+import "./style.css"
+
 // component = html + css + js
 const MyComponent = () => {
     return (
-        <div>Eric & HoiDanIT</div>
+        <>
+            <div>Eric & HoiDanIT</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
+
     );
 }
 
