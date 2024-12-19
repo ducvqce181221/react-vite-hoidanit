12f0edd369d0f5b583a9b8b6378eef3dcc72712b
Quang Duc <vuquangduc1404@gmail.com>
2024-12-19 17:10:22 +0700

    video36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -4,11 +4,22 @@ const TodoNew = (props) => {
     console.log("check props >>> ", props);
     const { addNewTodo } = props;
 
-    addNewTodo("Quang Duc");
+    // addNewTodo("Quang Duc");
+    const handleClick = () => {
+        alert("Click me!");
+    }
+
+    const handleOnChange = (name) => {
+        console.log("On Change!", name);
+    }
     return (
         <div className="todo-new">
-            <input type="text" placeholder="Enter anything you like" />
-            <button>Add</button>
+            <input type="text" placeholder="Enter anything you like"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                onClick={handleClick}
+            >Add</button>
         </div>
     );
 
