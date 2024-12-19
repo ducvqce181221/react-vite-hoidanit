0da0fffd30c6c9e5f5e49392a771d74e372ef166
Quang Duc <vuquangduc1404@gmail.com>
2024-12-19 16:25:51 +0700

    video34

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,22 @@ import reactLogo from "./assets/react.svg";
 
 const App = () => {
 
+  const name = "Eric";
+  const age = 25;
+  const data = {
+    city: "rachgia",
+    country: "vietnam"
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo list</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={name}
+        age={age}
+        data={data}
+      />
       <div className="todo-image">
         <img src={reactLogo} className="logo react" />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,9 +1,13 @@
 
 
-const TodoData = () => {
+const TodoData = (props) => {
+    const { name, age, data } = props;
+
+    console.log("check props >>> ", props);
     return (
         <div className="todo-data">
             <div>Learning React</div>
+            <div>My name is {name}</div>
         </div>
     );
 
