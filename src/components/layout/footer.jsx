b89463f57da5d62b14e5996d33e52be54219b264
Quang Duc <vuquangduc1404@gmail.com>
2024-12-22 12:06:16 +0700

    video47

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,12 @@
+import '../layout/footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>React Vite</p>
+        </div>
+
+    );
+}
+
+export default Footer;
