fa0f16afa6c1009f748563ecb1dbe5240ff5220f
Quang Duc <vuquangduc1404@gmail.com>
2024-12-29 21:13:02 +0700

    video90

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,32 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, notification } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
     const [form] = Form.useForm();
+    const navigate = useNavigate();
 
-    const onFinish = (values) => {
+    const onFinish = async (values) => {
         console.log(">>>values: ", values)
+
+        const res = await registerUserAPI(
+            values.fullName,
+            values.email,
+            values.password,
+            values.phone);
+
+        if (res.data) {
+            notification.success({
+                message: "Register User",
+                description: "Register user successful!"
+            });
+            navigate("/login");
+        } else {
+            notification.error({
+                message: "Register User",
+                description: res.message
+            });
+        }
     }
 
     return (
@@ -22,12 +44,12 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Full Name"
                         name="fullName"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your full name!',
+                            },
+                        ]}
                     >
                         <Input />
                     </Form.Item>
@@ -35,12 +57,12 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Email"
                         name="email"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                        ]}
                     >
                         <Input />
                     </Form.Item>
@@ -48,12 +70,12 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Password"
                         name="password"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
                     >
                         <Input.Password />
                     </Form.Item>
@@ -61,18 +83,30 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Phone Number"
                         name="phone"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                        rules={[
+                            {
+                                // required: true,
+                                pattern: new RegExp(/\d+/g),
+                                message: "Wrong format!"
+                            },
+                        ]}
                     >
                         <Input />
                     </Form.Item>
 
                     <Button type="primary" onClick={() => form.submit()}>
                         Register
+                    </Button>
+                    <Button style={{ marginLeft: "10px" }}
+                        onClick={() => {
+                            form.setFieldsValue({
+                                fullName: "name test 1",
+                                email: "testdemo@gmail.com",
+                                password: 123456789,
+                                phone: 66778899
+                            })
+                        }}>
+                        Test
                     </Button>
                 </div>
             </Form>
